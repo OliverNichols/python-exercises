7a488271b649e073740a4f1aa5492a5bb260173e
Ollie Nichols <ollienichols22@gmail.com>
2021-04-23 10:59:58 +0000

    Solutions for day 2

---
+++ day-2/exercise-1.py
@@ -0,0 +1,7 @@
+my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+num = int(input("Give me a number! "))
+
+result = [x for x in my_list if x < num]
+
+print(result)
---
+++ day-2/exercise-2.py
@@ -0,0 +1,11 @@
+for i in range(1, 51):
+    
+    result = ''
+
+    if not i%3:
+        result += 'Fizz'
+
+    if not i%5:
+        result += 'Buzz'
+
+    print(result or i) # if there is a result, then use it, else use the number i
