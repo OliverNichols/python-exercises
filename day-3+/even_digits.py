e657869445604498e4699433d07b9ff97377551e
Ollie Nichols <ollienichols22@gmail.com>
2021-04-27 15:02:36 +0000

    Solution to even_digits exercise added

---
+++ day-3+/even_digits.py
@@ -0,0 +1,12 @@
+def even_digits(a:int, b:int) -> str:
+    if a > b: b, a = a, b # works if a > b
+    
+    output = []
+    for n in range(a, b+1):
+        for digit in str(n):
+            if digit == '-': continue # works for negative numbers too
+            elif int(digit)%2: break
+
+        else:
+            output.append(str(n))
+    return ','.join(output)
